ae7b7cc8fdd463579eeafa7da60ca2340e20a3a7
RaeInTech <11688089+RaeInTech@users.noreply.github.com>
2023-07-26 13:52:26 -0500

    Add better solution option and parameter names

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,20 +1,28 @@
-const convertToCelsius = function (degree) {
-  let farToCel = (degree - 32) * (5 / 9);
+const convertToCelsius = function (fahrenheit) {
+  let farToCel = (fahrenheit - 32) * (5 / 9);
 
   if (farToCel === 0) {
     return farToCel;
   } else {
     return Number.parseFloat(farToCel.toFixed(1));
+    /*
+    BETTER SOLUION: 
+     return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+     */
   }
 };
 
-const convertToFahrenheit = function (degree) {
-  let celToFar = degree * (9 / 5) + 32;
+const convertToFahrenheit = function (celsius) {
+  let celToFar = celsius * (9 / 5) + 32;
 
   if (celToFar === 0) {
     return celToFar;
   } else {
     return Number.parseFloat(celToFar.toFixed(1));
+    /*
+    BETTER SOLUION: 
+     return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+     */
   }
 };
 
