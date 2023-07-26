939f779664994f9d77baf33ab9a90bbc779e4d6f
RaeInTech <11688089+RaeInTech@users.noreply.github.com>
2023-07-26 13:45:03 -0500

    Add solutions to tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degree) {
+  let farToCel = (degree - 32) * (5 / 9);
+
+  if (farToCel === 0) {
+    return farToCel;
+  } else {
+    return Number.parseFloat(farToCel.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+  let celToFar = degree * (9 / 5) + 32;
+
+  if (celToFar === 0) {
+    return celToFar;
+  } else {
+    return Number.parseFloat(celToFar.toFixed(1));
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
