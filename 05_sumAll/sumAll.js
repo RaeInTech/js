b90c187b080a6551cbb319b1aff200c98ea769d9
RaeInTech <11688089+RaeInTech@users.noreply.github.com>
2023-07-26 13:26:00 -0500

    Add solutions to sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  let sum = 0;
 
+  if (!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0) {
+    return "ERROR";
+  }
+  //If min is greater than max, temporarily switch the values
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  for (let i = min; i <= max; i++) {
+    sum += i;
+    console.log(i);
+  }
+  return sum;
 };
 
 // Do not edit below this line
