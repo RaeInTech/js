673242810eff38922f8f2ce44760c6796780bba7
RaeInTech <11688089+RaeInTech@users.noreply.github.com>
2023-07-26 13:26:19 -0500

    Add solutions to leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  //year is divible by 4 but NOT by 100
+  //year is divisible by BOTH 100 and 400
 
+  return (
+    (year % 4 === 0 && year % 100 !== 0) ||
+    (year % 100 === 0 && year % 400 === 0)
+  );
 };
 
 // Do not edit below this line
